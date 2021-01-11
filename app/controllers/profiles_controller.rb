ff10769292f648da0bf3a3e6127903857cfd110f
yb270186-1716 <kaskawa1980@gmail.com>
2021-01-11 02:37:18 +0000

    update

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,7 @@
 class ProfilesController < ApplicationController
 
+  @@count = 0
+
   def index
     @profiles = Profile.where(user_id: current_user.id).last
 
@@ -7,40 +9,46 @@ class ProfilesController < ApplicationController
 
   def new
     @profiles = Profile.new  
-      
+    @count = Profile.count
+    @@count = @count
+    puts "--- new count ---"
+    puts @@count
+    
   end
 
   def show
     puts "-----profiles(show(params[:id]--"  
-    puts params[:id]  
-    @profiles = Profile.find(params[:id])
+    puts @@count
+    @profiles = Profile.find(@@count)
     
   end
 
   def edit
-    @profiles = Profile.find(params[:id])  
+    @profiles = Profile.find(@@count)  
   
   end
   
   def create
     # ストロングパラメーターを使用
-    ans = Profile.new(profile_params)
+    ans = Profile.new(id:@@count)
     ans.user_id = current_user.id
     # DBへ保存する
     ans.save
     # トップ画面へリダイレクト
-    redirect_to  profile_path(ans.user_id)
+    redirect_to  profile_path(id:@@count)
     puts "---- profiles(ans.user_id) ----"
     puts ans.user_id
+    puts ans.capa
+    puts ans.expe
     
     
   end
   
   def update
-   ans = Profile.find(params[:id])
+   ans = Profile.find(@@count)
    ans.user_id = current_user.id
    ans.update(profile_params)
-　 redirect_to  profile_path(params[:id])
+　 redirect_to  profile_path(@@count)
     
   end
   
@@ -50,7 +58,7 @@ class ProfilesController < ApplicationController
   
   private
   def profile_params
-   params.require(:profile).permit(:id, :user_id, :capa, :exep, :most1, :most2, :most3, :most4, :profile )
+   params.require(:profile).permit(:id, :user_id, :capa, :expe, :most1, :most2, :most3, :most4, :profile )
   end
   
 
